958a99a80acd5970916b82beeb76fb67aa790bbc
Felipe Alves <fajreyy.bh@gmail.com>
2018-11-12 15:45:25 -0200

    commit

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>Primeiro Paragrafo {props.name}</p>
+            <p>Segundo paragrafo</p>
+        </div>
+    )
+}
+
+export default userOutput;
+
